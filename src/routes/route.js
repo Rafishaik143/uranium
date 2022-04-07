269a2fd45eef5a46c52b9e182607996ca67ef1bb
Rafishaik143 <smrafir@gmail.com>
2022-04-07 10:09:11 +0530

    my first assingment

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+router.get('/test-me1', function (req, res) {
+    res.send('My second ever api2')
+});
 
 module.exports = router;
 // adding this comment for no reason
